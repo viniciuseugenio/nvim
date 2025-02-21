1dda6c9e4dadfe11b5051ab17393e528ee7746e4
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-02-21 19:04:56 -0300

    update plugins and remove copilot native plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -20,12 +20,4 @@ return {
     "echasnovski/mini.bracketed",
     version = false,
   },
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.api.nvim_set_keymap("i", "<C-j>", "copilot#Accept('<CR>')", { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<Tab>", "copilot#Accept('<Tab>')", { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-e>", "copilot#Dismiss()", { silent = true, expr = true })
-    end,
-  },
 }
